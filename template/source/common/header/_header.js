717532819215c5d1101d94626aacd391082653cb
Michał Sobiecki <michalsobiecki@yahoo.com>
2017-02-21 12:44:00 +0100

    Init

---
+++ template/source/common/header/_header.js
@@ -0,0 +1,53 @@
+(function () {
+  var header = {
+    init: function () {
+      this.catchDOM();
+      if (this.isElement()) {
+        this.checkHeader();
+      }
+    },
+    catchDOM: function () {
+      this.$el = $('.js-header');
+      this.$wrapper = this.$el.find('.header__content');
+      this.$content = this.$el.find('.header__box');
+    },
+    isElement: function () {
+      return this.$el.length > 0
+    },
+    checkHeader: function () {
+      if (this.$el.hasClass('-header-fixed')) {
+        if (this.$el.hasClass('-header-landingPage')) {
+          this.setLandingPage();
+        }
+        else {
+          this.setFixedMenu();
+        }
+      }
+    },
+    setFixedMenu: function () {
+      this.$el.css('min-height', this.$content.height());
+      this.$wrapper.css('min-height', this.$content.height());
+    },
+    setLandingPage: function () {
+      this.$el.css('min-height', 0);
+      this.$wrapper.css('min-height', 0);
+      this.setStateHeaderBackground();
+    },
+    setStateHeaderBackground: function () {
+      $(window).scroll(this.setHeaderBackground.bind(this))
+    },
+    setHeaderBackground: function () {
+      if ($(window).scrollTop() > this.$content.height()) {
+        if (!this.$el.hasClass('-header-landingPage-background')) {
+          this.$el.addClass('-header-landingPage-background')
+        }
+      } else {
+        if (this.$el.hasClass('-header-landingPage-background')) {
+          this.$el.removeClass('-header-landingPage-background')
+        }
+      }
+    }
+  };
+
+  header.init();
+})();
---
+++ template/source/components/banner/_banner.js
@@ -0,0 +1,143 @@
+/*
+ * Banner version v.1.0 (Fullsceen)
+ *
+ * Components
+ * html: _banner.pug
+ * css: _banner.sass
+ *
+ * Init a fullscreen banner
+ * @class .js-banner
+ *
+ * Possible modification
+ * # Create a slick-carousel
+ * @class .js-banner.js-banner-slick
+ * # If Fixed header (modification into .header e.g. [.header.-header-fixed] also [.header.-header-landingPage])
+ * @class
+ *
+ * @author: https://github.com/SaliMike
+ */
+
+(function () {
+  var banner = {
+    init: function () {
+      this.catchDOM();
+      if (this.isElement()) {
+        if (this.isSlickInit()) {
+          this.generateSlick();
+        }
+        this.setHeight();
+        this.onResize();
+        this.toggleHide();
+        this.toggleEvent();
+      }
+    },
+    catchDOM: function () {
+      this.$el = $('.js-banner');
+      this.$slider = this.$el.find('.banner__slider');
+      this.$item = this.$el.find('.banner__item');
+      this.$image = this.$el.find('.banner__image');
+      this.$toggler = this.$el.find('.banner__toggler');
+      this._windowHeight = $(window).height();
+
+      this.$header = $('.js-header');
+      this.$headerBox = this.$header.find('.header__box');
+      this._headerHeight = this.$header.height();
+    },
+    isElement: function () {
+      return this.$el.length > 0
+    },
+    isSlickInit: function () {
+      return this.$el.hasClass('js-banner-slick')
+    },
+
+    //SLICK
+    generateSlick: function () {
+      this.$slider.slick({
+        slide: '.banner__item',
+        infinite: true,
+        dots: true,
+        appendDots: '.banner__dots',
+        customPaging: function (slider, i) {
+          return '<div class="banner__dot"></div>';
+        },
+        arrows: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        adaptiveHeight: false,
+        autoplay: true,
+        speed: 3000
+      });
+      this.$item = this.$el.find('.banner__item');
+      this.$image = this.$el.find('.banner__image');
+      this.onInit();
+      this.onChangeSlide();
+    },
+    onInit: function () {
+      this.$slider.find('.slick-current .banner__content').addClass('-banner-content-show animated fadeIn');
+    },
+    onChangeSlide: function () {
+      this.$slider.on('afterChange', function (event) {
+        var _this = $(event.target).find('.slick-current .banner__content');
+        $(event.target).find('.banner__content').not(_this).removeClass('-banner-content-show animated fadeIn');
+        _this.addClass('-banner-content-show animated fadeIn');
+      });
+    },
+
+    //OnResize
+    onResize: function () {
+      $(window).resize(this.setNewVariables.bind(this))
+    },
+    setNewVariables: function () {
+      this._windowHeight = $(window).height();
+      this._headerHeight = $('header').height();
+      this.setHeight();
+    },
+    setHeight: function () {
+      if ($('.header.-header-landingPage.-header-fixed').length > 0) {
+        this.$el.css('height', this._windowHeight);
+        this.$slider.css('height', this._windowHeight);
+        this.$item.css('height', this._windowHeight);
+        this.$image.css('height', this._windowHeight);
+      } else {
+        this.$el.css('height', this._windowHeight - this._headerHeight);
+        this.$slider.css('height', this._windowHeight - this._headerHeight);
+        this.$item.css('height', this._windowHeight - this._headerHeight);
+        this.$image.css('height', this._windowHeight - this._headerHeight);
+      }
+    },
+    toggleHide: function () {
+      this.$item.each(function () {
+        if ($(this).attr('href')) {
+          $(this).find('.banner__toggler').css('display', 'none')
+        } else {
+          $(this).find('.banner__toggler i').addClass('animated infinite pulse');
+        }
+      })
+    },
+    toggleEvent: function () {
+      this.$toggler.on('click', this.toggleScroll.bind(this))
+    },
+    toggleScroll: function (event) {
+      event.preventDefault();
+      var target = $(event.currentTarget).data('href');
+      var height = this.checkHeader();
+      $('html, body').animate({
+        scrollTop: ($(target).offset().top - height)
+      }, 1500);
+    },
+    checkHeader: function () {
+      if (this.$header.length > 0) {
+        if (this.$header.hasClass('-header-fixed')) {
+          return this.$headerBox.height();
+        } else {
+          return 0
+        }
+      }
+      else {
+        return 0
+      }
+    }
+  };
+
+  banner.init();
+})();
